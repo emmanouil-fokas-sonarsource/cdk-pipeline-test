37776df9bd7d7dfcc295101736c025b303c5342f
Emmanouil Fokas <emmanouil.fokas@sonarsource.com>
2025-03-20 15:13:12 +0100

    Autoscan test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pipelinePOC import PipelinePOCStack
 
 app = aws_cdk.App()
 PipelinePOCStack(app, "CDKPipelinePOC",
-    env=aws_cdk.Environment(account="111111111111", region="eu-central-1")
+    env=aws_cdk.Environment(account="1211111111111", region="eu-central-1")
 )
 
 app.synth()
